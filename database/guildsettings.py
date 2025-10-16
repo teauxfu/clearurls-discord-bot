3bd205c903d83680268099ecd02431c4b67b2320
teauxfu <teauxfu@icloud.com>
2025-10-15 21:27:57 -0500

    simplify guild settings

--- database/guildsettings.py
+++ database/guildsettings.py
@@ -4,41 +4,35 @@ import sqlite3
 def ensure_settings_table(con: sqlite3.Connection) -> None:
     sql = """
     create table if not exists 
-    GuildSettings_DeleteAndRepostDirtyMessages (guild_id int primary key, is_enabled bit);
+    guild_settings (guild_id int primary key, replace_messages bit);
     """
     cur = con.cursor()
     cur.execute(sql)
     cur.close()
 
-def ensure_guild_automod_setting(con: sqlite3.Connection, guild_id: int) -> None:
-    sql = """
-    insert into GuildSettings_DeleteAndRepostDirtyMessages (guild_id, is_enabled) 
-    select :id, :is_enabled
-    where not exists (
-        select 1 from GuildSettings_DeleteAndRepostDirtyMessages
-        where guild_id = :id
-    )
-    """
-    cur = con.cursor()
-    cur.execute(sql, {"id": guild_id, "is_enabled": False})
-    cur.close()
-
 def get_automod_setting(con: sqlite3.Connection, guild_id: int) -> bool:
     sql = """
-    select coalesce(is_enabled, 0) from GuildSettings_DeleteAndRepostDirtyMessages
+    select coalesce(replace_messages, 0) from guild_settings
     where guild_id = :id
     """
     cur = con.cursor()
     res = cur.execute(sql, {"id": guild_id})
     return res.fetchone() == (1,)
 
-def set_automod_setting(con: sqlite3.Connection, guild_id: int, enabled: bool) -> None:
-    sql = """
-    update GuildSettings_DeleteAndRepostDirtyMessages 
-    set is_enabled = :enabled
-    where guild_id = :id
+def set_automod_setting(con: sqlite3.Connection, guild_id: int, replace_messages: bool) -> None:
+    # https://www.sqlite.org/draft/lang_UPSERT.html
+    sql_upsert = """
+    insert into guild_settings (guild_id, replace_messages) 
+    values (:guild_id, :replace_messages)
+
+    on conflict(guild_id)
+    do 
+        update set replace_messages = :replace_messages
+        where guild_id = :guild_id
     """
+
     cur = con.cursor()
-    cur.execute(sql, {"enabled": enabled, "id": guild_id})
+    param = {"replace_messages": replace_messages, "guild_id": guild_id}
+    cur.execute(sql_upsert, param)
     cur.close()
 
