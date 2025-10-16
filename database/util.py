fd94191c2e529bccc265cdb30da34b7fb898bc69
teauxfu <teauxfu@icloud.com>
2025-10-15 21:28:07 -0500

    remove junk

--- database/util.py
+++ database/util.py
@@ -1,10 +1,7 @@
 import os
 import sqlite3
-from typing import Sequence
-from discord import Guild
 
-from auditlog import compact_deleted_messages_table, ensure_deleted_messages_table
-from guildsettings import ensure_guild_automod_setting, ensure_settings_table
+from database.guildsettings import ensure_settings_table
 
 def get_dbcon() -> sqlite3.Connection:
     # the extra params enable use of timestamp columns and datetime datatypes
@@ -12,9 +9,5 @@ def get_dbcon() -> sqlite3.Connection:
                              detect_types=sqlite3.PARSE_DECLTYPES |
                              sqlite3.PARSE_COLNAMES)
 
-def initialize_db(con: sqlite3.Connection, guilds: Sequence[Guild]) -> None:
+def initialize_db(con: sqlite3.Connection) -> None:
         ensure_settings_table(con)
-        ensure_deleted_messages_table(con)
-        compact_deleted_messages_table(con)
-        for guild in guilds:
-            ensure_guild_automod_setting(con, guild.id)
